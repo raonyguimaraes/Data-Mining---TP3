0590694b5771a62a416c57406d3ede7fdce746d7
Raony Guimaraes <raonyguimaraes@gmail.com>
2011-10-14 20:08:29 -0300

    changes

--- parse2.py
+++ parse2.py
@@ -11,14 +11,11 @@ from prettytable import PrettyTable
 from nltk.metrics.distance import jaccard_distance, masi_distance
 
 DISTANCE_THRESHOLD = 0.34
+#DISTANCE_THRESHOLD = 0.34
 DISTANCE = masi_distance
 
 # Import the goods
 from cluster import HierarchicalClustering
-
-# Define a scoring function
-def score(music1, music2):
-  return DISTANCE(set(music1), set(music2))
 
 
 def clean_string(string):
@@ -32,7 +29,7 @@ def clean_string(string):
   
   return clean_string.strip()
 
-data_file = open('tags.txt', 'rb')
+data_file = open('tags2.txt', 'rb')
 data = data_file.readlines()
 
 fdist = nltk.FreqDist()
@@ -71,19 +68,12 @@ print str(len(musics))+" musics"
 #sort_list = fdist.keys()
 #print sort_list
 
-################Cluster tags
-#print "Clustering Tags"
-#clusters = {}
-#for tag1 in all_tags:
-  #clusters[tag1] = []
-  #for tag2 in all_tags:
-    #if tag2 in clusters[tag1] or clusters.has_key(tag2) and tag1 in clusters[tag2]:
-      #continue
-    #distance = DISTANCE(set(tag1.split()), set(tag2.split()))
-    #if distance < DISTANCE_THRESHOLD:
-      #clusters[tag1].append(tag2)
 
 print "Clustering Musics"      
+
+# Define a scoring function
+def score(music1, music2):
+  return DISTANCE(set(music1), set(music2))
 
 # Feed the class your data and the scoring function
 hc = HierarchicalClustering(musics, score)
@@ -93,38 +83,51 @@ clusters = hc.getlevel(DISTANCE_THRESHOLD)
 # Remove singleton clusters
 clusters = [c for c in clusters if len(c) > 1]
 
-## Flatten out clusters
-#clusters = [clusters[tag] for tag in clusters if len(clusters[tag]) > 1]
-## Round up musics who are in these clusters and group them together
-#clustered_musics = {}
-#for cluster in clusters:
-  #clustered_musics[tuple(cluster)] = []
-  #for idx, music in enumerate(musics):
-    #for tag in music:
-      #if tag in cluster:
-	#clustered_musics[tuple(cluster)].append('Music nº %s' % (idx))
 
-print "Done"
-#print "Printing Clusters"
-##############Printing Cluster Created
-#for tags in clustered_musics:
-  #common_tags_heading = 'Common Tags: ' + ', '.join(tags)
-  #print common_tags_heading
-  #descriptive_terms = set(tags[0].split())
-  #for tag in tags:
-    #descriptive_terms.intersection_update(set(tag.split()))
-    #descriptive_terms_heading = 'Descriptive Terms: ' + ', '.join(descriptive_terms)
-  #print descriptive_terms_heading
-  #print '-' * max(len(descriptive_terms_heading), len(common_tags_heading))
-  #print '\n'.join(clustered_musics[tags])
-  #print
+######## End: HAC ########
 
-#calculate distance between musics
-#for each music calculate distance beetween this with all the others
-#matrix of distance
+# Round up musics who are in these clusters and group them together
+
+clustered_musics = {}
+
+for cluster in clusters:
+  clustered_musics[tuple(cluster)] = []
+  for idx, music in enumerate(musics):
+    for tag in music:
+      if tag in cluster:
+	clustered_musics[tuple(cluster)].append('%s' % (idx))
+	
+	
+
+json_output = {}
+for tags in clustered_musics:
+
+    descriptive_terms = set(tags[0].split())
+    for title in tags:
+        descriptive_terms.intersection_update(set(title.split()))
+
+    json_output[', '.join(descriptive_terms)[:30]] = dict([(c, None) for c in
+            clustered_musics[tags]])
 
 
-#cluster
+if not os.path.isdir('out'):
+    os.mkdir('out')
 
+# HTML_TEMPLATE references some dependencies that we need to
+# copy into out/
 
+shutil.rmtree('out/protovis-3.2', ignore_errors=True)
 
+shutil.copytree('../web_code/protovis/protovis-3.2',
+                'out/protovis-3.2')
+
+html = open(HTML_TEMPLATE).read() % (json.dumps(json_output),)
+f = open(os.path.join(os.getcwd(), 'out', OUT), 'w')
+f.write(html)
+f.close()
+
+print 'Data file written to: %s' % f.name
+
+# Open up the web page in your browser
+
+webbrowser.open('file://' + f.name)
