2e5a46cbf74c3bbf1aa5713b2dd83e56a4f3f4b3
Raony Guimaraes <raonyguimaraes@gmail.com>
2011-10-18 18:13:24 -0200

    changes

---
+++ topic_extraction.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+## -*- coding: utf-8 -*-
+
+# Author: Olivier Grisel <olivier.grisel@ensta.org>
+# License: Simplified BSD
+
+from time import time
+from sklearn.feature_extraction import text
+from sklearn import decomposition
+from sklearn import datasets
+
+n_samples = 1000
+n_features = 1000
+n_topics = 10
+n_top_words = 20
+
+# Load the 20 newsgroups dataset and vectorizer it using the most common word
+# frequency with TF-IDF weighting (without top 5% stop words)
+
+t0 = time()
+print "Loading dataset and extracting TF-IDF features..."
+dataset = datasets.fetch_20newsgroups(shuffle=True, random_state=1)
+
+vectorizer = text.CountVectorizer(max_df=0.95, max_features=n_features)
+counts = vectorizer.fit_transform(dataset.data[:n_samples])
+tfidf = text.TfidfTransformer().fit_transform(counts)
+print "done in %0.3fs." % (time() - t0)
+
+# Convert scipy.sparse.csr_matrix representation to dense numpy array
+tfidf = tfidf.toarray()
+
+# Remove negative values (they should not be there, is this a bug?)
+tfidf[tfidf < 0] = 0.0
+
+# Fit the NMF model
+print "Fitting the NMF model on with n_samples=%d and n_features=%d..." % (
+    n_samples, n_features)
+nmf = decomposition.NMF(n_components=n_topics).fit(tfidf)
+print "done in %0.3fs." % (time() - t0)
+
+# Inverse the vectorizer vocabulary to be able
+inverse_vocabulary = dict((v, k) for k, v in vectorizer.vocabulary.iteritems())
+
+for topic_idx, topic in enumerate(nmf.components_):
+    print "Topic #%d:" % topic_idx
+    print " ".join([inverse_vocabulary[i]
+                    for i in topic.argsort()[:-n_top_words:-1]])
+    print
